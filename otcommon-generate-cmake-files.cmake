d7502839fffa6011eb3517c491f92d3dd71dbb1d
Justus Ranvier <justus@opentransactions.org>
2023-05-17 16:05:35 -0500

    otcommon_generate_cmake_files: make install location configurable

--- otcommon-generate-cmake-files.cmake
+++ otcommon-generate-cmake-files.cmake
@@ -3,38 +3,37 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-function(otcommon_generate_cmake_files template_file)
+function(
+  otcommon_generate_cmake_files
+  template_file
+  modules_dir
+  install_location
+)
   include(CMakePackageConfigHelpers)
-  set(MODULES_DIR "cmake")
-  set(${PROJECT_NAME}_CMAKE_DEST
-      "${CMAKE_INSTALL_LIBDIR}/${MODULES_DIR}/${PROJECT_NAME}"
-  )
 
   configure_package_config_file(
     "${template_file}"
-    "${${PROJECT_NAME}_BINARY_DIR}/${MODULES_DIR}/${PROJECT_NAME}Config.cmake"
-    INSTALL_DESTINATION ${${PROJECT_NAME}_CMAKE_DEST}
-    PATH_VARS MODULES_DIR
+    "${${PROJECT_NAME}_BINARY_DIR}/${modules_dir}/${PROJECT_NAME}Config.cmake"
+    INSTALL_DESTINATION ${install_location}
+    PATH_VARS modules_dir
     NO_SET_AND_CHECK_MACRO NO_CHECK_REQUIRED_COMPONENTS_MACRO
   )
-
   write_basic_package_version_file(
-    "${${PROJECT_NAME}_BINARY_DIR}/${MODULES_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    "${${PROJECT_NAME}_BINARY_DIR}/${modules_dir}/${PROJECT_NAME}ConfigVersion.cmake"
     VERSION ${${PROJECT_NAME}_VERSION_STRING}
     COMPATIBILITY AnyNewerVersion
   )
 
   install(
     FILES
-      "${${PROJECT_NAME}_BINARY_DIR}/${MODULES_DIR}/${PROJECT_NAME}Config.cmake"
-      "${${PROJECT_NAME}_BINARY_DIR}/${MODULES_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
-    DESTINATION ${${PROJECT_NAME}_CMAKE_DEST}
+      "${${PROJECT_NAME}_BINARY_DIR}/${modules_dir}/${PROJECT_NAME}Config.cmake"
+      "${${PROJECT_NAME}_BINARY_DIR}/${modules_dir}/${PROJECT_NAME}ConfigVersion.cmake"
+    DESTINATION ${install_location}
     COMPONENT ${PROJECT_NAME}_cmake_modules
   )
-
   install(
     EXPORT ${PROJECT_NAME}
-    DESTINATION ${${PROJECT_NAME}_CMAKE_DEST}
+    DESTINATION ${install_location}
     NAMESPACE "${PROJECT_NAME}::"
   )
 endfunction()
